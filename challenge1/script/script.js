52abd0fc6ebf2267e94a46c1bb8a1c2bf5435176
Prabuddha <prabuddhagope2001@gmail.com>
2020-07-22 06:33:44 +0000

    Complete project

--- challenge1/script/script.js
+++ challenge1/script/script.js
@@ -1,15 +1,23 @@
 function showShareSection()
 {
     element = document.getElementById("share-section");
+    share = document.getElementById("share-icon");
+    section = document.getElementById("share-icon-and-section");
     visibility = element.style.visibility;
 
-    if (visibility == 'hidden')
+
+    if (visibility == 'visible')
     {
-        element.style.visibility = 'visible';
+        element.style.visibility = 'hidden';
+        share.style.background = '#ECF2F8';
+        share.style.color = '#708298';
     }
     else
     {
-        element.style.visibility = 'hidden';
+        element.style.visibility = 'visible';
+        share.style.background = '#708298';
+        share.style.color = '#fff';
     }
+
 
 }
